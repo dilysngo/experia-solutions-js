611fa2d4786b6a2f6195c36ab0f268080a0caeb3
thanhthien <thanhthienbui123@gmail.com>
2019-09-17 18:07:39 +0700

    update sdk facebook

--- plugins/event-bus.js
+++ plugins/event-bus.js
@@ -4,7 +4,7 @@ import Vue from 'vue';
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    js.src = "//connect.facebook.com/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
